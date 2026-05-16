f660885739f910d8e229c848a761c05bf7939bf7
edu-hiroshi <eduhito57@gmail.com>
2026-05-16 08:21:51 -0300

    feat: added ManyToOne and OneToMany relationships to models

--- src/main/java/com/eduhi/bookstore/models/PublisherModel.java
+++ src/main/java/com/eduhi/bookstore/models/PublisherModel.java
@@ -1,7 +1,10 @@
 package com.eduhi.bookstore.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -15,6 +18,13 @@ public class PublisherModel implements Serializable {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    // used Set instead of a List due to large amount of relationships as hibernate can be
+    // unable to handle a high amount of data
+    // WRITE_ONLY avoids serialization errors due to Lazy fetch only loading data when needed
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
+    private Set<BookModel> books = new HashSet<>();
 
     public String getName() {
         return name;
@@ -30,5 +40,13 @@ public class PublisherModel implements Serializable {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public Set<BookModel> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<BookModel> books) {
+        this.books = books;
     }
 }
